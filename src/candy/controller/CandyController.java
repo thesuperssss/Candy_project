f5544b4d9fd610f10e9e73f57bbf45e2e76a62de
thesuperssss <thesuperssss@Live.com>
2014-01-21 13:25:03 -0700

    update

---
+++ src/candy/controller/CandyController.java
@@ -0,0 +1,17 @@
+package candy.controller;
+
+//import candy.model.MarshmallowCreature;
+
+public class CandyController
+{
+	//private MarshmallowCreature myCreature;
+	public void start()
+	{
+		 makeMonster();
+	}
+	private void makeMonster()
+	{
+		//myCreature = new MarshmallowCreature("Billybobfredjoejackson Jr. the first", true, 2, 5, 1);
+	}
+
+}
---
+++ src/candy/controller/CandyRunner.java
@@ -0,0 +1,10 @@
+package candy.controller;
+
+public class CandyRunner
+{
+	public static void main(String[] args)
+	{
+		//CandyController myController = new CandyController();
+	}
+
+}
